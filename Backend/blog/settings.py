8cd6f6c424470498ef21109c7e1ebbd4f24f539b
amrit-GH23 <amritkumar2005btw@gmail.com>
2025-04-05 22:04:13 +0530

    changed the database

--- Backend/blog/settings.py
+++ Backend/blog/settings.py
@@ -95,12 +95,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 
