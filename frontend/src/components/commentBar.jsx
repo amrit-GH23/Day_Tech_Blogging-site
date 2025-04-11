7a7b7e12e995d027227f4e731860793ac19f6ced
amrit-GH23 <amritkumar2005btw@gmail.com>
2025-04-11 13:31:53 +0530

    edited comment bar

--- frontend/src/components/commentBar.jsx
+++ frontend/src/components/commentBar.jsx
@@ -2,17 +2,18 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 const CommentBar = (props) => {
- 
-  const [comment,setComment]=useState([]);
+
+  const { comment, author, created_at } = props.comm;
+
 
   return (
     <div>
        <div>
-        <p className="text-sm text-black mb-3">By {props.author}</p>
+        <p className="text-sm text-black mb-3">By {author}</p>
        <h2 className="text-2xl font-semibold text-blue-800 mb-2">
-          {props.comment}
+          {comment}
         </h2>
-        <p className="text-sm text-gray-500 mb-3">{new Date(props.created_at).toLocaleDateString()}</p>
+        <p className="text-sm text-gray-500 mb-3">{new Date(created_at).toLocaleDateString()}</p>
        </div>
     </div>
   )
